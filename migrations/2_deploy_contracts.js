c1535cf1e7f1f265662920557943e9810f8c01a4
Lucian Hymer <lucian.hymer@gmail.com>
2022-06-01 20:27:05 -0700

    Starting React dev

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Whine = artifacts.require("./Whine.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(Whine);
 };
