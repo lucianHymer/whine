38e2427215c3153dafbcb428cc37f9af139c0c47
Lucian Hymer <lucian.hymer@gmail.com>
2022-06-06 10:04:53 -0700

    Restart with a bare truffle unbox react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import Whine from "./contracts/Whine.json";
+import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { bottle: {}, web3: null, accounts: null, whine: null };
+  state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -17,15 +17,15 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetworkWhine = Whine.networks[networkId];
-      const whine = new web3.eth.Contract(
-        Whine.abi,
-        deployedNetworkWhine && deployedNetworkWhine.address,
+      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        SimpleStorageContract.abi,
+        deployedNetwork && deployedNetwork.address,
       );
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, whine }, this.runExample);
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -36,18 +36,19 @@ class App extends Component {
   };
 
   runExample = async () => {
-    const { accounts, whine } = this.state;
+    const { accounts, contract } = this.state;
 
-    await whine.methods.drinkSomethingEpoch().send({ from: accounts[0] });
-    console.log('awaited');
+    // Stores a given value, 5 by default.
+    await contract.methods.set(5).send({ from: accounts[0] });
 
-    const bottle = await whine.methods.bottles(0).call();
-    this.setState({bottle});
-    console.log('Bottle', bottle);
+    // Get the value from the contract to prove it worked.
+    const response = await contract.methods.get().call();
+
+    // Update state with the result.
+    this.setState({ storageValue: response });
   };
 
   render() {
-    console.log("TEST0");
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
@@ -56,14 +57,14 @@ class App extends Component {
         <h1>Good to Go!</h1>
         <p>Your Truffle Box is installed and ready.</p>
         <h2>Smart Contract Example</h2>
-        <div>
-          <table><tbody>
-            <tr><th>ID</th><td>{this.state.bottle && this.state.bottle.id}</td></tr>
-            <tr><th>Name</th><td>{this.state.bottle && this.state.bottle.name}</td></tr>
-            <tr><th>Brand</th><td>{this.state.bottle && this.state.bottle.brand}</td></tr>
-            <tr><th>Vintage</th><td>{this.state.bottle && this.state.bottle.vintage}</td></tr>
-          </tbody></table>
-        </div>
+        <p>
+          If your contracts compiled and migrated successfully, below will show
+          a stored value of 5 (by default).
+        </p>
+        <p>
+          Try changing the value stored on <strong>line 42</strong> of App.js.
+        </p>
+        <div>The stored value is: {this.state.storageValue}</div>
       </div>
     );
   }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-var Whine = artifacts.require("./Whine.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
-  deployer.deploy(Whine);
 };
