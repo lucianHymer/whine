debacf2618eb6c840f512fef25976c6eed7d4233
Lucian Hymer <lucian.hymer@gmail.com>
2022-06-06 13:35:49 -0700

    Working sample with hardhat and react

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Whine from "./contracts/Greeter.sol/Greeter.json";
+import { abi as whineAbi } from "./contracts/Greeter.sol/Greeter.json";
+import { networks as whineNetworks } from "./contracts/Greeter.sol/network.json";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { useWeb3React } from '@web3-react/core'
 import Web3 from 'web3'
@@ -8,35 +9,47 @@ import logo from './logo.svg';
 import './App.css';
 
 const MetamaskWallet = new InjectedConnector({
-  supportedChainIds: ['31337'],
+  supportedChainIds: [31337],
 });
 
-function App() {
-  const { activate, account, chainId, library } = useWeb3React();
+function App(){
+  const { activate, account, chainId, library, error } = useWeb3React();
   const [ whineContract, setWhineContract ] = useState(null);
+  const [ greeting, setGreeting ] = useState(null);
 
   useEffect( () => {
-    try {
-      if(library){
-        console.log('Account', account);
+    (async () => {
+      try {
+        if(error) console.log(error);
 
-        const web3 = new Web3(library.provider);
+        if(library && !whineContract){
+          console.log('Account', account);
+          console.log('Networks', whineNetworks);
+          console.log('ABI', whineAbi);
 
-        // Get the contract instance.
-        const deployedNetwork = Whine.networks[chainId];
-        setWhineContract(new web3.eth.Contract(
-          Whine.abi,
-          deployedNetwork && deployedNetwork.address,
-        ));
+          const web3 = new Web3(library.provider);
+
+          // Get the contract instance.
+          const deployedNetwork = whineNetworks[chainId];
+          setWhineContract(new web3.eth.Contract(
+            whineAbi,
+            deployedNetwork && deployedNetwork.address,
+          ));
+        }
+        if(whineContract) {
+          const result = await whineContract.methods.greet().call({ from: account });
+          setGreeting(result);
+        }
+      } catch (error) {
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`,
+        );
+      console.error(error);
       }
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-    console.error(error);
-    }
-  }, [account, chainId, library]);
+    })();
+  }, [error, account, chainId, library, whineContract]);
+
 
   return (
     <div className="App">
@@ -55,6 +68,8 @@ function App() {
         </a>
         <br />
         <button onClick={() => activate(MetamaskWallet) }>Connect Metamask Wallet</button>
+        <br />
+        Greeting: {greeting || (error ? error.message : null)}
       </header>
     </div>
   );
--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -4,6 +4,7 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -18,6 +19,18 @@ async function main() {
   const greeter = await Greeter.deploy("Hello, Hardhat!");
 
   await greeter.deployed();
+
+  const chainId = greeter.provider._network.chainId;
+  const { address } = greeter;
+  const content = JSON.stringify({ networks: {
+    [chainId]: {address}
+  }});
+  const path = './artifacts/contracts/Greeter.sol/network.json';
+  fs.writeFileSync(path, content, err => {
+    if(err) console.log(err);
+  });
+  console.log(`Wrote ${content} to ${path}`);
+
 
   console.log("Greeter deployed to:", greeter.address);
 }
