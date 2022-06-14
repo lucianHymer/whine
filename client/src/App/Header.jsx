8c31ad01e3f605e25c7baa9407e211789516d759
Lucian Hymer <lucian.hymer@gmail.com>
2022-06-14 07:02:44 -0700

    Responsive wallet button

--- client/src/App/Header.jsx
+++ client/src/App/Header.jsx
@@ -1,9 +1,11 @@
+import React, { Fragment } from "react";
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { useWeb3React } from '@web3-react/core'
 import { 
   Flex,
   Spacer,
   Button,
+  Text,
 } from '@chakra-ui/react';
 import { HamburgerIcon } from '@chakra-ui/icons';
 
@@ -11,14 +13,39 @@ const MetamaskWallet = new InjectedConnector({
   supportedChainIds: [31337],
 });
 
+const WalletButtonText = () => {
+  const { account } = useWeb3React();
+
+  if(account)
+    return (
+      <Fragment>
+        <Spacer />
+        <Text color='green.300'>{"\u2B24"}</Text>
+        <Spacer />
+        Connected
+        <Spacer />
+      </Fragment>
+    );
+  else
+    return (
+      <Fragment>
+        <Spacer />
+        Connect Wallet
+        <Spacer />
+      </Fragment>
+    );
+};
+
 const Header = () => {
-  const { activate } = useWeb3React();
+  const { account, activate } = useWeb3React();
 
   return (
     <Flex h="10%">
       <Spacer />
-      <Button m={2} size='md' onClick={() => activate(MetamaskWallet) }>
-        Connect Wallet
+      <Button m={2} size='md' onClick={() => account ? "" : activate(MetamaskWallet) }>
+        <Flex w="8em">
+          <WalletButtonText />
+        </Flex>
       </Button>
       <Button m={2} size='md' onClick={() => alert("Under Development") }>
         <HamburgerIcon />
